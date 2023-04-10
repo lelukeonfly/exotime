fca2c7e8dfc32db58ea692493d05a413693086dc
lukas.moessler@gmail.com <lukas.moessler@gmail.com>
2023-04-11 01:16:57 +0200

    feat: optimization for add cat

--- resources/js/Pages/Posts/Edit.vue
+++ resources/js/Pages/Posts/Edit.vue
@@ -3,7 +3,7 @@ import AppLayout from '../../Layouts/AppLayout.vue';
 import Service from '../../Components/Service.vue';
 import Demand from '../../Components/Demand.vue';
 
-import {Link, useForm} from '@inertiajs/vue3';
+import {Link, useForm, router} from '@inertiajs/vue3';
 import { ref } from 'vue';
 
 const props = defineProps({
@@ -16,36 +16,10 @@ const form = useForm({
     description: ''
 })
 
-//const create_category = ref('')
-//const category = "this is cat";
-
-//todo: add categories
-
-//try->//https://larainfo.com/blogs/laravel-9-inertia-vue-3-crud-tutorial-example
 const addCategory = () => {
-    form.post(route('categories.store')).then(
-        response => {
-            if (response.status = 201){
-                console.log(201)
-            }else{
-                console.log(!201)
-            }
-        }
-    )
+    router.post(route('categories.store'), form)
 }
 
-//const addCategory = () => {
-  //form.post(route("categories.store"), form).then(response => {
-    //if (response.status === 201) {
-    //console.log('201111111')
-    //} else {
-    //console.log('nononononoo')
-    //}
-  //}).catch(error => {
-    //// Handle request error
-    //console.log('errorrr')
-  //});
-//}
 const usedCategories = props.post.categories.map(x => props.categories.findIndex(y => y.id === x.id));
 
 const isChecked = (index) => {
